6f86df246c839f4fecbe235de5edbca82a85cfda
Riahijihen <105865129+Riahijihen@users.noreply.github.com>
2023-12-05 23:26:15 +0100

    Add files via upload

---
+++ MyProjectApplication.java
@@ -0,0 +1,23 @@
+package com.jihen.MyProject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+
+import com.jihen.MyProject.entities.Client;
+import com.jihen.MyProject.entities.commande;
+
+@SpringBootApplication
+public class MyProjectApplication implements CommandLineRunner {
+@Autowired
+private RepositoryRestConfiguration repositoryRestConfiguration;
+public static void main(String[] args) {
+SpringApplication.run(MyProjectApplication.class, args);
+}
+@Override
+public void run(String... args) throws Exception {
+repositoryRestConfiguration.exposeIdsFor(commande.class ,Client.class);
+}
+}
